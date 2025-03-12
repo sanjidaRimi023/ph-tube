403fd455f71d568a641b97992b1e93e432cc07fa
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-03-12 21:54:49 +0600

    develop a ph tube app

---
+++ script/index.js
@@ -0,0 +1,11 @@
+function loadCategories() {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+        .then(res => res.json())
+        .then(data => displayCategories(data.categories))
+}
+
+function displayCategories(categories) {
+    console.log(categories);
+    
+}
+loadCategories();
